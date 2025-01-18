0c01ca2d82d81a5e1d8d7e6b81cc925f4a7aabbd
RushiVivek <rushivivek123@gmail.com>
2025-01-18 23:16:14 +0530

    .

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request
+from webAnalyzer import AccessibliltyAnalyzer
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+		url = request.form.get("website_link")
+		analyzer = AccessibliltyAnalyzer()
+        issues = analyzer.analyze_website(url)
+
+		return render_template("index.html", issues=issues)
+
 	return render_template("index.html")
 
 if __name__ == "__main__":
